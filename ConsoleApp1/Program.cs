6f76cb02cf6d96c2dfe1d978ac3ab42745df7d67
angela chaj <angelachaj648@gmail.com>
2026-03-15 16:51:36 -0500

    Estructura inicial del programa menu

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿//SIMULADOR DE DECISIONES PARA PLATAFORMA DE STREAMING
+int totalEvaluados = 0;
+int publicados = 0;
+int rechazados = 0;
+int enRevision = 0;
+int impactoAlto  = 0;
+int impactoMedio  = 0;
+int impactoBajo  = 0;
+
+void MostrarMenu()
+
+{
+	Console.WriteLine("Simulador de Streaming");
+	Console.WriteLine("1.evaluar nuevo contenido");
+	Console.WriteLine("2.mostrar reglas del sistema ");
+	Console.WriteLine("3.mosstra estadistica de la seccion");
+	Console.WriteLine("4.reiniciar estadisticas");
+	Console.WriteLine("5. salir");
+	Console.WriteLine("Seecione una opcion");
+}
+
+
+
+
